c74f85aeb1cd6dbeb1568e952bd11bcce74d6bec
integerwjo <wanderaotieno321@gmail.com>
2026-04-07 17:57:17 +0300

    refactor player module

--- src/player.rs
+++ src/player.rs
@@ -1,41 +1,25 @@
-//! This module represents a generic player which could be a human or the computer
-use crate::{gamestate::GameState, player};
+//! Represents a generic player (human or computer)
 
 use super::{Card, Number};
 
 #[derive(Debug)]
 pub struct Player {
-     pub cards_in_hand: Vec<Card>,
-     pub is_turn_to_play: bool
+    pub cards_in_hand: Vec<Card>,
+    pub is_turn_to_play: bool,
 }
 
-impl Player{
-
-    /// simple case for checking if a player is able to finish the game (kadi)
-    fn can_finish(player: &Self) -> bool {
-        if player.cards_in_hand.len() == 1 {
-            match player.cards_in_hand[0].number {
-                Number::Four | Number::Five | Number::Six | Number::Seven | 
-                Number::Nine | Number::Ten => true,
-                _ => false
-            }
-        } else {
-            false
-        }
+impl Player {
+    /// Returns true if the player can finish the game (kadi)
+    pub fn can_finish(&self) -> bool {
+        matches!(
+            self.cards_in_hand.as_slice(),
+            [Card { number: Number::Four
+                | Number::Five
+                | Number::Six
+                | Number::Seven
+                | Number::Eight
+                | Number::Nine
+                | Number::Ten, .. }]
+        )
     }
-
-
 }
-
-pub fn can_finish(player: &Player) -> bool {
-        if player.cards_in_hand.len() == 1 {
-            match player.cards_in_hand[0].number {
-                Number::Four | Number::Five | Number::Six | Number::Seven |
-                Number::Eight| Number::Nine | Number::Ten => true,
-                _ => false
-            }
-        } else {
-            false
-        }
-     }
-
