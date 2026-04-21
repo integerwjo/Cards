78cb502e8baaf96563d6e2362927dacb39386a7f
integerwjo <wanderaotieno321@gmail.com>
2026-04-21 17:43:28 +0300

    add player impl

--- src/player.rs
+++ src/player.rs
@@ -8,4 +8,18 @@ pub struct Player {
     pub is_turn_to_play: bool,
 }
 
-
+impl Player {
+    /// Returns true if the player can finish the game (kadi)
+    pub fn can_finish(&self) -> bool {
+        matches!(
+            self.cards_in_hand.as_slice(),
+            [Card { number: Number::Four
+                | Number::Five
+                | Number::Six
+                | Number::Seven
+                | Number::Eight
+                | Number::Nine
+                | Number::Ten, .. }]
+        )
+    }
+}
