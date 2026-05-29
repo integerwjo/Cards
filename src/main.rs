158c8108b74bcf027de2bc69099226ad88b2e32b
integerwjo <wanderaotieno321@gmail.com>
2026-05-29 18:53:02 +0300

    clean imports

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,44 @@
 //! Poker
-//! A binary crate for a version of poker played in kenya
+//! A binary crate for a Kenyan variation of poker.
 
 #![forbid(unsafe_code)]
 #![forbid(unsafe_op_in_unsafe_fn)]
 
 pub mod cardmodule;
 pub mod computer;
-pub mod human;
 pub mod gamestate;
+pub mod human;
 pub mod player;
 
-pub use crate::cardmodule::{Card, Deck, CardEffect};
-pub use rand::seq::SliceRandom;
-pub use rand::thread_rng;
-use crate::{gamestate::GameState};
-pub use gamestate::{ Number, Types };
+pub use crate::cardmodule::{Card, CardEffect, Deck};
+pub use gamestate::{GameState, Number, Types};
 pub use player::Player;
 
-/// This represents a player instance or an instance of the model the human player plays with
+pub use rand::seq::SliceRandom;
+pub use rand::thread_rng;
 
+/// Represents the game loop for the poker game.
 fn main() {
-     let mut game_state =  gamestate::GameState::new();
-     cardmodule::print_top_card(&game_state);
-     loop 
-     {       
-          human::player_turn(&mut game_state);
-          println!("Can human finish: {}", game_state.human_player.can_finish());
-          computer::computer_turn(&mut game_state);
-          cardmodule::print_top_card(&game_state);
-          println!("Can computer finish: {}", game_state.computer_player.can_finish());
-         
+    let mut game_state = GameState::new();
 
-     }
+    cardmodule::print_top_card(&game_state);
 
+    loop {
+        human::player_turn(&mut game_state);
+        println!(
+            "Can human finish: {}",
+            game_state.human_player.can_finish()
+        );
 
+        computer::computer_turn(&mut game_state);
+
+        cardmodule::print_top_card(&game_state);
+
+        println!(
+            "Can computer finish: {}",
+            game_state.computer_player.can_finish()
+        );
+    }
 }
+
+
